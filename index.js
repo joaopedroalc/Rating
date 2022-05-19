47d91e580ddc01910caf719e58ba35f6142cb809
joaopedrocode <joaopedro.ufma@gmail.com>
2022-05-19 14:10:28 -0300

    rating

---
+++ index.js
@@ -0,0 +1,24 @@
+const avaliacoes = [];
+const legendas = document.querySelectorAll('.legenda')
+const legendasArray = Array.from(legendas);
+
+const exportCSV = document.querySelector('.download-csv');
+
+legendasArray.map(span => {
+  const label = span.parentElement;
+  label.addEventListener('click', () => {
+    if (window.confirm('Tem certeza da sua Avaliação ?')) {
+      const avaliacao = span.innerHTML
+      avaliacoes.push(avaliacao)
+    }
+
+    const todasAvaliacoes = avaliacoes.join("\n")
+    console.log(todasAvaliacoes)
+    exportCSV.addEventListener('click', () => {
+      exportCSV.setAttribute('href', `data:text/csvcharset=utf-8,${encodeURIComponent(todasAvaliacoes)}`)
+
+      exportCSV.setAttribute('download', 'avaliacoes.csv')
+    })
+  })
+})
+
